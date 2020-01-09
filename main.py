f7f1bccb91290151f47248d650c10ff487d712ca
skvortsov <skvortsov.a04@gmail.com>
2020-01-09 05:17:03 +0300

    Добавлена БАЗОВАЯ загрузка уровней Добавлены картинки, используемые в коде Добавлен первый "уровень" для отладки

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import random
 import pygame
-from source.system_functions import load_image, terminate
+from source.system_functions import load_image, terminate, load_level
+
 # import system_functions
 
 FPS = 50
+tile_width, tile_height = 50, 50
 WIDTH = 1072
 HEIGHT = 603
 MENUBTTNS = [None for _ in range(4)]
@@ -16,6 +18,7 @@ clock = pygame.time.Clock()
 
 running = True
 iteration = 0
+
 
 def choose_mode(chosed=None):
     screen.fill((0, 0, 0))
@@ -41,13 +44,10 @@ def choose_mode(chosed=None):
         MODEBTTNS[i] = ((text_x, text_y, text_w, text_h), cr)
 
 
-
-
-
 def main_menu(chosed=None):
     screen.fill((0, 0, 0))
     font = pygame.font.Font(None, 50)
-    texts = ["Новая игра", "Продолжить","Настройки", "Выход"]
+    texts = ["Новая игра", "Продолжить", "Настройки", "Выход"]
     for i in range(4):
         cr = (100, 255, 100)
         if i == chosed:
@@ -59,7 +59,7 @@ def main_menu(chosed=None):
         text_y = (HEIGHT // 5) * i + text_h // 2 + HEIGHT // 10
         screen.blit(text, (text_x, text_y))
         pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
-                                           text_w + 20, text_h + 20), 1)
+                                               text_w + 20, text_h + 20), 1)
         MENUBTTNS[i] = ((text_x, text_y, text_w, text_h), cr)
 
 
@@ -70,7 +70,6 @@ def what_is_pressed(buttons, pos):
             return iteration
         print(i[0][2] + i[0][0], x, i[0][0], '\n',
               i[0][1] + i[0][3], y, i[0][1])
-
 
 
 def start_screen():
@@ -103,7 +102,6 @@ def start_screen():
                         flag = None
                     main_menu()
 
-
         if not in_menu:
             break
 
@@ -111,11 +109,14 @@ def start_screen():
         clock.tick(FPS)
     flag()
 
+
 def settings_screen():
     pass
 
+
 def continue_screen():
     pass
+
 
 def choose_mode_screen():
     choose_mode()
@@ -131,10 +132,8 @@ def choose_mode_screen():
                 btn = what_is_pressed(MODEBTTNS, (x, y))
                 if btn == 0:
                     flag = 1
-                    print(btn)
                 elif btn == 1:
                     flag = 2
-                    print(btn)
                 else:
                     flag = game
                 choose_mode(btn)
@@ -159,12 +158,31 @@ def choose_mode_screen():
         clock.tick(FPS)
     flag()
 
+
+def generate_level(level):
+    green_spawnpoint, red_spawnpoint = None, None  # Точки появления для игроков
+    enemies_spawnpoints = []  # список с точками появления проивников
+
+    for y in range(1, len(level), 2):
+        for x in range(1, len(level[y]), 2):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == 'G':
+                green_spawn = (x, y)
+            elif level[y][x] == 'R':
+                red_spawn = (x, y)
+            elif level[y][x] == 'E':
+                enemies_spawnpoints.append((x, y))
+    return green_spawnpoint, red_spawnpoint, enemies_spawnpoints
+
+
 def game():
     pass
 
-start_screen()
 
-#tile_images = {'empty': load_image('grass.png')}
+tile_images = {'empty': load_image('grass.png'), 'wall': load_image('box.png')}
 player_image = load_image('tank_green_mk1.png')
 
 # группы спрайтов
@@ -172,5 +190,14 @@ all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+
+
+start_screen()
 
 pygame.quit()
